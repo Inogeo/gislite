f5ee6167bd20e6235f20c6f9436685adc252efa5
Pierre HARDY <pierre.hardy@egis.fr>
2022-03-28 18:07:15 +0200

    feat(MainBody): cretion of MainBody element

--- app/gislite/src/index.js
+++ app/gislite/src/index.js
@@ -8,24 +8,12 @@ import '../node_modules/uikit/dist/css/uikit.css';
 import '../node_modules/uikit/dist/js/uikit.js';
 
 // React app writing
-class GisLiteBody extends React.Component {
-    render() {
-        return (
-            <div className="uk-grid-collapse uk-child-width-expand@s uk-text-center uk-margin-large-top" data-uk-grid>
-                <div>
-                    <div className="uk-background-muted uk-padding">Item</div>
-                </div>
-                <div>
-                    <div className="uk-background-primary uk-padding uk-light">Item</div>
-                </div>
-            </div>
-        );
-    }
-}
+import MainBody from './react-components/MainBody.jsx'
 
 // ========================================
-
 ReactDOM.render(
-    <GisLiteBody />,
+    <MainBody />,
     document.getElementById('root')
 );
+
+
